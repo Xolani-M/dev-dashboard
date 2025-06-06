72a975b633ba5bbd6baa58b2e707ca772de8bcc9
Xolani Mvana <86495928+Xolani-M@users.noreply.github.com>
2025-06-06 10:12:48 +0200

    Update FavoritesContext.tsx

--- src/context/FavoritesContext.tsx
+++ src/context/FavoritesContext.tsx
@@ -1,5 +1,5 @@
 // FavoritesContext.tsx
-import React, { createContext, useContext, useState, useEffect} from 'react';
+import { createContext, useContext, useState, useEffect} from 'react';
 import type { ReactNode } from 'react';
 
 // Define the structure of a GitHub user (for favorites)
